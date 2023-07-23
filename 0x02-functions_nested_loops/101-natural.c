b604a46ac456bdd1e70a097de8470f1f905f092c
lilie1 <addolillian12@gmial.com>
2023-07-23 16:53:54 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints count of multiples
+ * of 3 or 5 below 1024
+ * Return: return (0)
+ */
+int main(void)
+{
+int sum, i;
+
+sum = 0;
+for (i = 1; i < 1024; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+sum += i;
+}
+printf("%d\n", sum);
+return (0);
+}
