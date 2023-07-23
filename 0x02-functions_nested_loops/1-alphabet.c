05f46b58ebbcf8af181500eb12dd426e639b94f9
lilie1 <addolillian12@gmial.com>
2023-07-23 16:05:42 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description: a function to print alphabet in lowercase
+ * Return: Always 0. (success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
