6ed85504a4bc95926ee32b8e63c7e7bf3ea7f1c2
lilie1 <addolillian12@gmial.com>
2023-07-26 08:35:16 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strncpy - prints
+ * @dest: The character to print
+ * @src: The character to print
+ * @n: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ **/
+char *_strncpy(char *dest, char *src, int n)
+{
+int y = 0, src_len = 0;
+
+while (src[y++])
+src_len++;
+for (y = 0; src[y] && y < n; y++)
+dest[y] = src[y];
+for (y = src_len; y < n; y++)
+dest[y] = '\0';
+return (dest);
